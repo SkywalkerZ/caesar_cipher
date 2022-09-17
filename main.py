93e219c7fb99c0603423cf3693912c9ee1faa6a7
SkywalkerZ <abraarshk98@gmail.com>
2022-09-17 22:15:52 +0530

    first blood

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text +=char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}\n")
+
+from art import logo
+print(logo)
+
+looper = "yes"
+while looper == "yes":
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  if shift > 26:
+    shift=26-(shift % 26)
+    print(shift)
+    
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  
+  looper = input("Would you like to try again? Type 'yes' to continue, 'no' to exit\n").lower()
+  if looper == "yes":
+    os.system('clear')
