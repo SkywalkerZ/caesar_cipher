a8a4474dd82d0b2f638715c5341300628fbb4816
Abraar <abraarshk98@gmail.com>
2024-02-23 17:39:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-import os
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+is_active = True
 
-def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  for char in start_text:
-    if char in alphabet:
-      position = alphabet.index(char)
-      new_position = position + shift_amount
-      end_text += alphabet[new_position]
-    else:
-      end_text +=char
-    
-  print(f"Here's the {cipher_direction}d result: {end_text}\n")
+def caesar(c_text, c_shift, c_direction):
+    caesar_text = ''
+
+    for letter in c_text:
+        index = alphabet.index(letter)
+        if c_direction == 'encode':
+            if index + shift > 25:
+                caesar_text += alphabet[(index + c_shift) - 26]
+            else:
+                caesar_text += alphabet[index + c_shift]
+        else:
+            if index + shift < 0:
+                caesar_text += alphabet[26 - (index - c_shift)]
+            else:
+                caesar_text += alphabet[index - c_shift]
+
+    print(f'The {c_direction}d text is {caesar_text}')
 
 from art import logo
 print(logo)
 
-looper = "yes"
-while looper == "yes":
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
-  
-  if shift > 26:
-    shift=26-(shift % 26)
-    print(shift)
-    
-  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-  
-  looper = input("Would you like to try again? Type 'yes' to continue, 'no' to exit\n").lower()
-  if looper == "yes":
-    os.system('clear')
+while is_active:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction != 'encode':
+        if direction != 'decode':
+            print("Invalid input. Buh-bye!")
+            break
+
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(c_text=text, c_shift=shift, c_direction=direction)
+
+    choice = input("Would you like to continue? Type 'y' for YES or 'n' for NO:\n").lower()
+
+    if choice == 'n':
+        is_active = False
